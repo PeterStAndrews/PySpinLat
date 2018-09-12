e87d7b15f2114db87e6feca691aaadda260cc3f2
PeterStAndrews <pm78@st-andrews.ac.uk>
2018-09-12 15:41:58 +0100

    Add files via upload

---
+++ pyspinlat/__init__.py
@@ -0,0 +1,21 @@
+# initialisation for PySpinLat
+#
+# Copyright (C) 2018 Peter Mann
+# 
+# This file is part of `PySpinLat`, for statistical mechanical 
+# spin lattice models using Python.
+#
+# `PySpinLat` is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# `PySpinLat` is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with `PySpinLat`. If not, see <http://www.gnu.org/licenses/gpl.html>.
+
+from .lattice import LatticeProcess
